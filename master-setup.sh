59aa67b4f9a11155991520a4c25555ed8e7c1cbd
sanjuthere <sanjuthere.in.gmail.com>
2025-04-05 11:01:05 +0530

    modified

--- master-setup.sh
+++ master-setup.sh
@@ -8,7 +8,19 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# Deploy Calico network plugin (or you can use Flannel)
+# Wait for kube-apiserver to become available
+until kubectl get nodes &>/dev/null; do
+  echo "Waiting for API server to respond..."
+  sleep 5
+done
+
+# Wait until the master node is in Ready status
+until kubectl get nodes | grep -q 'Ready'; do
+  echo "Waiting for master node to become Ready..."
+  sleep 5
+done
+
+# Deploy Calico network plugin
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml
 
 # Display the join command
